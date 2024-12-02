5a6571e1b5cba73829a3b72584f24ebff2116b8a
Patrick Meier <patrick.meier@komaxgroup.com>
2024-12-02 08:58:47 +0100

    Aufgabe 2-3 done

--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -3,14 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	// TODO: create a map called "modules"
+	// create a map called "modules"
+	modules := map[int]string{
+		104: "Introduction to Go",
+		117: "Advanced Go",
+		346: "Go Concurrency",
+	}
 
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
-	// TODO: delete one
-	// TODO: add one
-	// TODO: replace one
+	// delete one
+	delete(modules, 117)
+
+	// add one
+	modules[200] = "Go Testing"
+
+	// replace one
+	modules[346] = "Go Concurrency 2"
+
 	fmt.Println(modules)
 }
