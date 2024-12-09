1f76ffbd9830011d032d40ac76298fdc55cd5234
Patrick Meier <patrick.meier@komaxgroup.com>
2024-12-09 09:18:28 +0100

    Aufgabe 4-1 done

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,13 @@
 package main
 
-// TODO: implement the function computeGrade
+import "fmt"
+
+
+func computeGrade(points, maxpoints float64) float64 {
+	return (points / maxpoints) * 5 + 1
+}
 
 func main() {
-	// TODO: call the function computeGrade
+	fmt.Println(computeGrade(10, 10)) 
+	fmt.Println(computeGrade(19, 30))
 }
