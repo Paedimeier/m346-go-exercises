75502a5e13564a1b7f3fbaf88d9f1c6e98c09804
Patrick Meier <patrick.meier@komaxgroup.com>
2024-12-09 09:26:01 +0100

    Aufgabe 4-2 done

--- go-4-ex-2/main.go
+++ go-4-ex-2/main.go
@@ -1,7 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 // TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
+
+func computeHypotenuse(kathete1, kathete2 float64) float64 {
+	return math.Sqrt(math.Pow(kathete1, 2) + math.Pow(kathete2, 2))
+}
 
 func main() {
 	// TODO: call the function computeHypotenuse
+	fmt.Println(computeHypotenuse(3, 4))
+	fmt.Println(computeHypotenuse(5, 12))
 }
